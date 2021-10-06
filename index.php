4a0465de66d7bf6556936bc0f74877ce1e621dc0
sazzzonov <my15032001@yandex.ru>
2021-10-06 20:40:59 +0300

    new com

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<h1> Заголовок страницы</h1>
+  
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title> Лабораторная работа №1 </title>
+		<link rel="stylesheet" href="mycss.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "Результат вычисления площади круга с радиусом 12 равен: ";
+				echo 3,14*12*12;
+			?>
+		</div>
+	</body>
+</html>
